6701cc38e2c83184318f95c40efab9103925aca2
Martin <ntmbohorquez@gmail.com>
2022-09-11 11:10:18 -0500

    cambie  toda la apariencia

---
+++ MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MascotaFeliz.App.Dominio;
+namespace MascotaFeliz.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Chrisian Calvache!");
+        }
+    }
+}
